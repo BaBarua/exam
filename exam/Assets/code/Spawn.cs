da27d9d2660d1e0d8dc6dd0c9b3cccacb102cb93
BaBarua <92506158+BaBarua@users.noreply.github.com>
2022-05-31 12:42:59 +0700

    Update Spawn.cs

--- exam/Assets/code/Spawn.cs
+++ exam/Assets/code/Spawn.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public int numberTs;
+
+    public List<GameObject> spawnpool;
+
+    public GameObject quad;
+ 
     void Start()
     {
-        
+        SpawnObj();
     }
 
-    // Update is called once per frame
+    public void SpawnObj()
+    {
+
+        int randomItem = 0;
+        GameObject toSpawn;
+        MeshCollider c  = quad.GetComponent<MeshCollider>();
+
+        float screenX, screenY;
+        Vector2 pos;
+
+        for(int i = 0; i < numberTs; i++)
+        {
+            randomItem = Random.Range(0, spawnpool.Count);
+            toSpawn = spawnpool[randomItem];
+
+            screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
+            screenY = Random.Range(c.bounds.min.x, c.bounds.max.x);
+            pos = new Vector2(screenX, screenY);
+
+            Instantiate(toSpawn, pos, toSpawn.transform.rotation);
+        }
+    }
+    
+
     void Update()
     {
         
